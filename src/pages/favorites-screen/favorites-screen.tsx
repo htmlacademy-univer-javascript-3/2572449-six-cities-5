b2872e575890f869cbe3297d7a2c0eaf536dabbc
Nastya <nastya@yandex.ru>
2024-12-08 12:10:30 +0300

    Истина где-то на сервере

--- src/pages/favorites-screen/favorites-screen.tsx
+++ src/pages/favorites-screen/favorites-screen.tsx
@@ -1,44 +1,16 @@
-import { Offer } from '../../types/offer';
 import { Link } from 'react-router-dom';
 import { typeOfCardList } from '../../utils';
 import OfferList from '../../components/offer-list/offer-list';
+import { useAppSelector } from '../../hooks';
+import Header from '../../components/header/header';
+import { AppRoute } from '../../const';
 
-type FavoritesScreenProps = {
-  offers: Offer[];
-};
 
-function FavoritesScreen({offers}: FavoritesScreenProps): JSX.Element {
+function FavoritesScreen(): JSX.Element {
+  const favoriteOffers = useAppSelector((state) => state.offers).filter((offer) => offer.isFavorite);
   return (
     <div className="page">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Link to="/" className="header__logo-link">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-              </Link>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
-
+      <Header />
       <main className="page__main page__main--favorites">
         <div className="page__favorites-container container">
           <section className="favorites">
@@ -52,15 +24,15 @@ function FavoritesScreen({offers}: FavoritesScreenProps): JSX.Element {
                     </a>
                   </div>
                 </div>
-                <OfferList offers={offers} listType={typeOfCardList.favourites} />
+                <OfferList offers={favoriteOffers} listType={typeOfCardList.favourites} />
               </li>
             </ul>
           </section>
         </div>
       </main>
       <footer className="footer container">
-        <Link to="/" className="header__logo-link">
-          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33"/>
+        <Link to={AppRoute.Main} className="header__logo-link">
+          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33" />
         </Link>
       </footer>
     </div>
